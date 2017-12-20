3c89a91d4e0ded973fcb1cef650b4b2c15a43f83
Jester-2-6 <33711287+Jester-2-6@users.noreply.github.com>
2017-12-20 15:14:45 +0530

    wtf?

--- donorHome.php
+++ donorHome.php
@@ -16,13 +16,39 @@
         <div>
             <h4>Your current items</h4>
 
-            <?php
-            require_once("Database.php");
-            require_once("Item.php");
 
+            <?php
+
+            require_once('Item.php');
+            require_once('Database.php');
+
+            $requested = false;
+            $category = "";
             $db = new Database();
-            $db->select('user', '*');
-            $db->makeTable(['User ID', 'PWD', 'name', 'add', 'phone', 'email']);
+
+            if (array_key_exists("requested", $_GET)) {
+                $requested = ($_GET["requested"] != "true");
+                echo $category;
+                $category = $_GET['category'];
+                $keywordString = $_GET['keywordString'];
+
+                $itemList = Item::returnAvailItems($category, $keywordString, $db);
+
+
+                echo "<h4>Following items are available in :" . ucwords($category) . "</h4>";
+
+                try {
+                    $db->makeTable(
+                        ['Item ID', 'Title', 'Description', 'Donor', 'Category', 'Date', 'Request'], [3, 5, 7], null,
+                        $itemList, true, 'request', "Request", ""
+                    );
+                } catch (DatabaseException $e) {
+                    $e->echoDetails();
+                }
+
+            }
+
+
             ?>
         </div>
     </tr>
