e30d8ab10f003f4c653af48bacaf6021e7bc5eb6
Jester-2-6 <33711287+Jester-2-6@users.noreply.github.com>
2017-12-24 17:40:33 +0530

    grid UI improvements

--- admin_Home.php
+++ admin_Home.php
@@ -5,6 +5,15 @@ require_once ("Request.php");
 require_once ("Item.php");
 
 
+echo "
+<html lang=\"en\">
+<head>
+    <meta charset=\"UTF-8\">
+    <title>Home | Admin</title>
+    <link rel=\"stylesheet\" type=\"text/css\" href=\"css/home.css\">
+</head>
+<body>";
+
 try
 {
     $db = new Database();
@@ -12,49 +21,23 @@ try
     $status = Recipient::ALLOWED_STATUSES[2];
     $userList = Recipient::returnList($db, $status);
 
-    echo "
-<html lang=\"en\">
-<head>
-    <meta charset=\"UTF-8\">
-    <title>Home | Admin</title>
-    <link rel=\"stylesheet\" type=\"text/css\" href=\"css/home.css\">
-</head>
-<body>
-<header>
-            <h2>Welcome ";
-echo $name;
-
-echo "</h2>
-        </header>";
-
-
     echo "<h4>Sent Items to be Received</h4>";
 
     $db = new Database();
     $requestsListConfirmed = Request::returnConfirmedRequests($db);
 
-    $db->makeTable(["Request ID", "Item ID", "Title", "Status", "Requester ID", "Summary", "View Request"],
+    $db->makeTable(["Request ID", "Item ID", "Title", "Status", "Requester ID", "Summary", "View"],
         [3,4,5,6,7,9,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],
         null, $requestsListConfirmed, true, "view",
-        "View Request", "donor_viewRequest.php"
+        "View", "admin_viewRequest.php"
     );
 
 
     echo "<h4>Following are the waiting users</h4>";
 
-    $db->makeTable
-    (
-    // 0user_id 1pwd 2status 3date_created 4name 5address 6phone 7email 8-- 9nic 10-- 11age 12occupation  13place of work 14salary 15proofdoc 16summary
-
-    ["user_id", "date_created", "name", "phone", "email", "age", "salary" ], [1,2,5,7,8,9,12,13,15,16,17], null,
-        $userList, true, 'details', "FullDetails", "admin_viewUserDetails.php"
-    );
-
     $db = new Database();
-
-    $status = Recipient::ALLOWED_STATUSES[1];
+    $status = Recipient::ALLOWED_STATUSES[2];
     $userList = Recipient::returnList($db, $status);
-    echo "<h4>Following are the reported users</h4>";
 
     $db->makeTable
     (
@@ -63,8 +46,25 @@ echo "</h2>
         ["user_id", "date_created", "name", "phone", "email", "age", "salary" ], [1,2,5,7,8,9,12,13,15,16,17], null,
         $userList, true, 'details', "FullDetails", "admin_viewUserDetails.php"
     );
-echo "</body>";
 
+
+
+    echo "<h4>Following are the reported users</h4>";
+    $db = new Database();
+
+    $status = Recipient::ALLOWED_STATUSES[1];
+    $userList = Recipient::returnList($db, $status);
+
+    $db->makeTable
+    (
+    // 0user_id 1pwd 2status 3date_created 4name 5address 6phone 7email 8-- 9nic 10-- 11age 12occupation  13place of work 14salary 15proofdoc 16summary
+
+        ["user_id", "date_created", "name", "phone", "email", "age", "salary" ], [1,2,5,7,8,9,12,13,15,16,17], null,
+        $userList, true, 'details', "FullDetails", "admin_viewUserDetails.php"
+    );
+
+echo "</body>
+</html>";
 
 } catch (DatabaseException $e) {
     $e->echoDetails();
